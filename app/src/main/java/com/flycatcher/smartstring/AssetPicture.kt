71e1de0adc1c465ed454be541187e811f8c17962
Velychko Dmitry <mitoza@gmail.com>
2024-02-22 15:41:51 +0200

    Add style and preview pictures to assets

---
+++ app/src/main/java/com/flycatcher/smartstring/AssetPicture.kt
@@ -0,0 +1,17 @@
+package com.flycatcher.smartstring
+
+enum class AssetPicture(val filename: String) {
+    SMILE_GIRL("SmileGirl.png"),
+    DAVID_1300("David1300.jpg"),
+    FLYCATCHER_LOGO("FlyCatcherLogo.png"),
+    GREEK_WOMAN("GreekWoman.jpg"),
+    GREEK_WOMAN_CROPPED("GreekWomanCropped.jpg"),
+    ICON_GIFT("IconGift.png"),
+    ICON_ROCKET("IconRocket.png"),
+    MORENA_BACCARIN("MorenaBaccarin.jpg"),
+    KID_DRAWING("KidDrawing.png"),
+    PHILIPINE_EAGLE("PhillipineEagle.jpg"),
+    PROJECTOR("SmartSketcherProjector.png"),
+    SHORT_HAIR("ShortHair.jpg"),
+    UNNAMED_STAR("UnnamedStar.jpg"),
+}
--- app/src/main/java/com/flycatcher/smartstring/MainActivity.kt
+++ app/src/main/java/com/flycatcher/smartstring/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.flycatcher.smartstring
 
+import android.content.res.AssetManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Picture
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.flycatcher.smartstringart.R
 import com.flycatcher.smartstringart.databinding.ActivityMainBinding
+import java.io.IOException
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +23,22 @@ class MainActivity : AppCompatActivity() {
         jniBridge = JniBridge()
         binding.mainText.text = jniBridge.showText("Hello Jni")
 
+        binding.preview.setImageBitmap(getPicture(AssetPicture.SMILE_GIRL))
+
+    }
+
+    fun getPicture(picture: AssetPicture): Bitmap {
+        var bitmap: Bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565)
+        var inputStream: InputStream? = null
+        try {
+            inputStream = assets.open(picture.filename)
+            bitmap = BitmapFactory.decodeStream(inputStream)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            inputStream?.close()
+        }
+        return bitmap
     }
 
 }
